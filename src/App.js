7bb7b6c34353044165ab485f301044cafbd40a47
EyYoTony <aswanon076@gmail.com>
2017-08-01 14:56:46 -0400

    started app page mockup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './pages/home'
 import MapPage from './pages/map-page'
+import AppMockup from './mockups/app-mockup'
 
 const App = props => {
   return (
@@ -9,6 +10,7 @@ const App = props => {
       <div>
         <Route exact path="/" component={Home} />
         <Route exact path="/map" component={MapPage} />
+        <Route exact path="/mockups/app-mockup" component={AppMockup} />
       </div>
     </BrowserRouter>
   )
---
+++ src/mockups/app-mockup.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AppMockup = () => {
+  return (
+    <div className="flex flex-column justify-between vh-100 w-100 avenir bg-washed-green">
+      <header className="flex flex-row justify-between items-center bg-dark-green h3 b f3 fw6 pa3">
+        <div>{'<'}</div>
+        <div>Street Art Search</div>
+        <div className="mr2">edit</div>
+      </header>
+    </div>
+  )
+}
+
+export default AppMockup
