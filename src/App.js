1af75d1d97a452870e43d9c3a9936cf8815c74a5
EyYoTony <aswanon076@gmail.com>
2017-08-07 10:49:44 -0400

    added a profile page mockup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './pages/home'
 import MapPage from './pages/map-page'
 import AppMockup from './mockups/app-mockup'
+import ProfileMockup from './mockups/profile-mockup'
 
 const App = props => {
   return (
@@ -11,6 +12,7 @@ const App = props => {
         <Route exact path="/" component={Home} />
         <Route exact path="/map" component={MapPage} />
         <Route exact path="/mockups/app-mockup" component={AppMockup} />
+        <Route exact path="/mockups/profile-mockup" component={ProfileMockup} />
       </div>
     </BrowserRouter>
   )
